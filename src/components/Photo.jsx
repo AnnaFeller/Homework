67c31bdd990e294d70aaa883de64e91b0a3b1e09
Anna_F <felleranna31@gmail.com>
2025-07-20 13:13:43 +0300

    Homework1

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react';
+const Photo = ({picture, isZoomed, onClick}) => {
+    let styles = 'w-full h-auto p-1 cursor-pointer transition-all duration-300 ease-in-out';
 
-const Photo = ({ picture, pos }) => {
-    const [isZoomed, setIsZoomed] = useState(false);
-
-    let styles = 'w-full p-1 cursor-pointer transition-transform  ease-in-out';
-    if (pos === 7) styles += ' rounded-bl-3xl';
-    if (pos === 9) styles += ' rounded-br-3xl';
-    if (isZoomed) { // если увеличиваем то оно будет таких размеров
-        styles += ' scale-175 z-10 relative'; //scale=маштаб
+    if (isZoomed) {
+        styles = ' w-full h-full justify-center';
     }
-
-    const click = () => { // был true станет false и наоборот
-        setIsZoomed(!isZoomed);
-    };
 
     return (
         <img
             className={styles}
             src={picture}
             alt=""
-            onClick={click}
+            onClick={onClick}
         />
     );
 };
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,14 +1,31 @@
 import {friends} from "../utils/constant.js";
 import Photo from "./Photo.jsx";
+import {useState} from "react";
 
 const Team = () => {
+    const [zoomed, setZoom] = useState(null);
+
+    const click = (index) => {
+        if (zoomed === index) {
+            setZoom(null);
+        } else {
+            setZoom(index);
+        }
+    }
+
+
     return (
         <section className="w-full max-w-4xl mx-auto border border-yellow-500 rounded-b-2xl p-4">
             <div className="grid grid-cols-3 gap-2">
+                {friends.map((friend, index) => {
+                    if (zoomed !== null && zoomed !== index)
+                        return null
+                    return (
+                        <Photo picture={friend.src} key={index} isZoomed={setZoom === index}
+                               onClick={() => click(index)}/>
 
-                {friends.map((friend, index) => (
-                    <Photo picture={friend.src} pos={friend.pos} key={index} />
-                ))}
+                    )
+                })}
             </div>
 
         </section>
